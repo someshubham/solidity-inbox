5b8fcc38af2ac4412f91d4eec22579cc15588030
Shubham Jain <someshubham@gmail.com>
2022-12-20 19:04:15 +0530

    Initial Commit

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+
+const { abi, evm } = require('./compile')
+require('dotenv').config();
+
+provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    process.env.INFURA_URL,
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    console.log('Attempting to deploy from account ', accounts[0])
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+    
+    console.log('Contract deployed to ', result.options.address)
+    provider.engine.stop()
+}
+
+deploy()
+
+// Contract Address 0xF365f0142b3E9095a6481BffA3cAF23699C3eb27
